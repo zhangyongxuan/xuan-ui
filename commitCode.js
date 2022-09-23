aa81438210e8f82e89d0649f35fd5a1559dd28f3
zhangyongxuan <1966674622@qq.com>
2022-09-23 09:42:51 +0800

    ''

--- commitCode.js
+++ commitCode.js
@@ -1,36 +1,44 @@
 const exec = require('child_process').exec;
 const [path, filePath, ...rest] = process.argv;
 const commitMsg = rest.join(',');
-console.log(commitMsg);
-exec(` git add . `, (error, stdout, stderr) => {
+// console.log(commitMsg);
+// exec(` git add . `, (error, stdout, stderr) => {
 
-    if (error) {
-        console.error('error: ' + error);
-        return;
+//     if (error) {
+//         console.error('error: ' + error);
+//         return;
+//     }
+//     exec(`git commit -m '${commitMsg}'`, (error1) => {
+//         if (error1) {
+//             console.error('error: ' + error1);
+//             return;
+//         }
+//         exec(`git push origin master`, (error2) => {
+
+//             if (error2) {
+//                 console.error('error: ' + error2);
+//                 return;
+//             }
+//             exec(`git push gitee master`, (error3) => {
+//                 if (error3) {
+//                     console.error('error: ' + error3);
+//                     return;
+//                 }
+//             })
+//         })
+//     })
+// })
+
+const commitCode = async(m)=>{
+    try {
+        await exec('git add .');
+        await exec(`git commit -m '${commitMsg}'`);
+        await exec(`git push origin master`);
+        await exec(`git push gitee master`);
+    } catch (error) {
+        console.error(error);
     }
-    exec(`git commit -m '${commitMsg}'`, (error1) => {
-        if (error1) {
-            console.error('error: ' + error1);
-            return;
-        }
-        exec(`git push origin master`, (error2) => {
+}
 
-            if (error2) {
-                console.error('error: ' + error2);
-                return;
-            }
-            exec(`git push gitee master`, (error3) => {
-
-                if (error3) {
-                    console.error('error: ' + error3);
-                    return;
-                }
-            })
-        })
-    })
-})
-
-
-
-
+commitCode(commitMsg);
 // git add . ;git commit -m 'update';git push origin master;git push gitee master
