e44afe6fb220b40250afac98596a588fe228f1c0
zhangyongxuan <1966674622@qq.com>
2022-09-28 09:21:18 +0800

    ''

--- script/commit.js
+++ script/commit.js
@@ -1,5 +1,4 @@
 
-const exec = require('child_process').exec;
 const utils = require('./utils');
 
 const {execCommand} = utils;
@@ -12,13 +11,13 @@ const commitMsg = rest.join(',');
 const commitCode = async (m) => {
     try {
         const msg = await execCommand('git add .');
-        console.log(msg);
+        console.log(msg,1);
         const msg1 = await execCommand(`git commit -m '${commitMsg}'`);
-        console.log(msg1);
+        console.log(msg1,2);
         const msg2 = await execCommand(`git push origin master`);
-        console.log(msg2);
+        console.log(msg2,3);
         const msg3 = await execCommand(`git push gitee master`);
-        console.log(msg3);
+        console.log(msg3,4);
     } catch (error) {
         console.error(error);
     }
