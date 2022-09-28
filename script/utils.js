fd84e84ac4e2e0288fb4dbf927c1d85a26cf46f4
zhangyongxuan <1966674622@qq.com>
2022-09-28 09:22:31 +0800

    '123'

--- script/utils.js
+++ script/utils.js
@@ -2,6 +2,7 @@ const exec = require('child_process').exec;
 const execCommand = (cmd) => {
     return new Promise((resolve, reject) => {
         exec(cmd, (error, stdout, stderr) => {
+            
             if (error) {
                 reject(error);
             } else if (stderr) {
